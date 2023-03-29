0ab5ae739be78462ec6b1986ec3a9785334caf06
Alextrat <alejandro.perez@bootcamp-upgrade.com>
2023-03-29 16:07:58 +0200

    feat nav

---
+++ src/components/NavComponent/NavComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const NavComponent = () => (
+  <div>
+    NavComponent Component
+  </div>
+);
+
+NavComponent.propTypes = {};
+
+NavComponent.defaultProps = {};
+
+export default NavComponent;
