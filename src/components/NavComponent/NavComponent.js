4eb9c59132f29b1d3ade2c3a7885a327eb7ceed2
Rociliu <mrrosa.carrasco@gmail.com>
2023-04-13 21:01:52 +0200

    feat footer

--- src/components/NavComponent/NavComponent.js
+++ src/components/NavComponent/NavComponent.js
@@ -5,14 +5,14 @@ import styles from './NavComponent.module.css'
 
 
 const NavComponent = ({authenticated, logoutUser}) => (
-  <nav className={styles.nav}>
-    <ul>
-      <li><Link to ="/"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7cC9q7hasU8hELqLEBJZuP2cE0K0WokavUg&usqp=CAU"alt='home'></img></Link></li>
-      <li><Link to ="/gallery">Películas</Link></li>
-      <li><Link to ="/register">Regístrate</Link></li>
-      <li><Link to ="/private">Área Privada</Link></li>
-      {authenticated ? (<li><Link onClick={logoutUser}>| LogOut | </Link></li>) :
-      (<li><Link to ="/login">LogIn</Link></li>)}
+  <nav className={styles.Nav}>
+    <ul className={styles.NavUl}>
+      <li className={styles.NavLi}><Link className={styles.NavA} to ="/"><img className={styles.NavImg} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7cC9q7hasU8hELqLEBJZuP2cE0K0WokavUg&usqp=CAU"alt='home'></img></Link></li>
+      <li className={styles.NavLi}><Link className={styles.NavA} to ="/gallery">Películas</Link></li>
+      <li className={styles.NavLi}><Link className={styles.NavA} to ="/register">Regístrate</Link></li>
+      <li className={styles.NavLi}><Link className={styles.NavA} to ="/private">Área Privada</Link></li>
+      {authenticated ? (<li className={styles.NavLi}><Link className={styles.NavA} onClick={logoutUser}>| LogOut | </Link></li>) :
+      (<li className={styles.NavLi}><Link className={styles.NavA} to ="/login">LogIn</Link></li>)}
       
 
 
