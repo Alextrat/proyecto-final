b190ce35aa6138c571824b4a37c923f45f8bef09
Rociliu <mrrosa.carrasco@gmail.com>
2023-04-14 18:54:45 +0200

    feat estilos

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -16,29 +16,32 @@ const FooterComponent = () => {
 return (
 
     <footer className={styles.Footer}>
-            <div className={styles.FooterContainer}>
+        <div className={styles.FooterContainerP}>
+            <nav className={styles.FooterNav}>
+                    
+                    <p className={styles.FooterP}><a className={styles.FooterA} a href='/'>Contacto</a></p>
+                    <p className={styles.FooterP}><a className={styles.FooterA} a href='/'>Ayuda</a></p>
+                    <p className={styles.FooterP}><a className={styles.FooterA} a href='/'>Aviso Legal</a></p>
                 
-                    <nav className={styles.FooterNav}>
-                        <ul className={styles.FooterUl}>
-                            <li className={styles.FooterLi}><a className={styles.FooterA} a href='/'>Contacto</a></li>
-                            <li className={styles.FooterLi}><a className={styles.FooterA} a href='/'>Ayuda</a></li>
-                            <li className={styles.FooterLi}><a className={styles.FooterA} a href='/'>Aviso Legal</a></li>
-                        </ul>
-                    </nav>
+            </nav>
+                
             
-                    {/*  <nav className={styles.SocialNav}>
-                        <ul className={styles.SocialUl}>
-                            <li className={styles.SocialLi}><a className={styles.SocialA} a href='/'><FaFontAwesome icon={FaFacebook} /></a></li>
-                            <li className={styles.SocialLi}><a className={styles.SocialA} a href='/'><FaFontAwesome icon={FaInstagram} /></a></li>
-                            <li className={styles.SocialLi}><a className={styles.SocialA} a href='/'><FaFontAwesome icon={FaLinkedin} /></a></li>
-                            <li className={styles.SocialLi}><a className={styles.SocialA} a href='/'><FaFontAwesome icon={FaGithub} /></a></li>
-                        </ul>
+            
+        {/*  <nav className={styles.SocialNav}>
+            <ul className={styles.SocialUl}>
+                <li className={styles.SocialLi}><a className={styles.SocialA} a href='/'><FaFontAwesome icon={FaFacebook} /></a></li>
+                <li className={styles.SocialLi}><a className={styles.SocialA} a href='/'><FaFontAwesome icon={FaInstagram} /></a></li>
+                <li className={styles.SocialLi}><a className={styles.SocialA} a href='/'><FaFontAwesome icon={FaLinkedin} /></a></li>
+                <li className={styles.SocialLi}><a className={styles.SocialA} a href='/'><FaFontAwesome icon={FaGithub} /></a></li>
+            </ul>
             </nav> */ }
-            </div>
-            <div className={styles.CopyrightSection}>
-                <p className={styles.CopyrightP}>© {getCurrentYear()} All Rights Reserved</p>
-            </div>
             
+        </div>
+        
+        <div className={styles.CopyrightSection}>
+            <p className={styles.CopyrightP}>© {getCurrentYear()} All Rights Reserved</p>
+        </div>
+        
         </footer>
     )
 }
