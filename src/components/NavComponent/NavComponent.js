d4b8b06ff0f7a07a4c9f8d537af4942f2fbe6b57
Rociliu <mrrosa.carrasco@gmail.com>
2023-04-15 20:29:23 +0200

    Logo

--- src/components/NavComponent/NavComponent.js
+++ src/components/NavComponent/NavComponent.js
@@ -5,6 +5,12 @@ import styles from './NavComponent.module.css'
 
 
 const NavComponent = ({authenticated, logoutUser}) => (
+
+  <div className={styles.NavBar}>
+    <div className={styles.Logo}>
+      <img src='https://i0.wp.com/cineypalomitas.com/wp-content/uploads/2022/11/cropped-CINE-Y-PALOMITAS-scaled-1.jpg?fit=2549%2C1810&ssl=1'alt="logo"></img>
+    </div>
+  
   <nav className={styles.Nav}>
     <ul className={styles.NavUl}>
       <li className={styles.NavLi}><Link className={styles.NavA} to ="/"><img className={styles.NavImg} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7cC9q7hasU8hELqLEBJZuP2cE0K0WokavUg&usqp=CAU"alt='home'></img></Link></li>
@@ -19,6 +25,7 @@ const NavComponent = ({authenticated, logoutUser}) => (
 
     </ul>
   </nav>
+  </div>
 );
 
 NavComponent.propTypes = {};
